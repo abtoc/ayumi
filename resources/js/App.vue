ab814abc5cb1981ffa2b64343b7dca13659a7be6
Toshio Abe <abtoc88@gmail.com>
2024-10-04 17:16:55 +0900

    Vue3とVuetify3を組み込み

---
+++ resources/js/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+const drawer = ref(false)
+
+</script>
+
+<template>
+    <v-app>
+        <v-system-bar color="secondary">
+            System Bar
+        </v-system-bar>
+
+        <v-app-bar color="primary">
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+            <v-app-bar-title>
+                Application Bar
+            </v-app-bar-title>
+        </v-app-bar>
+
+        <v-navigation-drawer permanennt v-model="drawer">
+            Navigation Drawer
+        </v-navigation-drawer>
+
+        <v-main>
+            <v-container>
+                Main Contents
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
