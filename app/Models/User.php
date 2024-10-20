cf4cef0a2e01c6eb2f1335d94815cae21f30e2d9
Toshio Abe <abtoc88@gmail.com>
2024-10-20 19:46:52 +0900

    デプロイ用に調整

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -52,5 +54,11 @@ class User extends Authenticatable
     public function screenshots(): HasMany
     {
         return $this->hasMany(Screenshot::class);
+    }
+
+    // Filament
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
