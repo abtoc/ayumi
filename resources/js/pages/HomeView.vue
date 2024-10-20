955a0a3fa4ef51117d95cbc280a3cc1883d08929
Toshio Abe <abtoc88@gmail.com>
2024-10-20 13:44:38 +0900

    登録ライバー一覧実装

--- resources/js/pages/HomeView.vue
+++ resources/js/pages/HomeView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import VisitComponent from '../components/VisitComponent.vue';
 import ScreenshotComponent from '../components/ScreenshotComponent.vue';
+import LogoutComponent from '../components/LogoutComponent.vue';
 import MainLayout from '../layouts/MainLayout.vue';
 import { VPullToRefresh } from 'vuetify/labs/VPullToRefresh'
 import { useLoginState } from '../stores/LoginState';
@@ -20,24 +21,10 @@ const renderKey = ref(0)
 
 async function load({done}) {
     console.log('loading')
-    await new Promise(resolve => setTimeout(() => resolve(), 2000))
     renderKey.value = renderKey.value + 1
+    await new Promise(resolve => setTimeout(() => resolve(), 2000))
     console.log('load finish')
     done('ok')
-}
-
-const logout = () => {
-    axios.get('/sanctum/csrf-cookie')
-        .then((res) => {
-            axios.post('/api/logout'
-            ).then((res) => {
-                console.log(res)
-                st.logout()
-                drawer.value = false
-            }).catch((err) => {
-                console.log('logout error:'+err)
-            })
-        })
 }
 
 </script>
@@ -46,8 +33,9 @@ const logout = () => {
 <MainLayout title="Ayumi System">
     <template v-slot:navigation>
         <v-list-item to='/' title="ホーム"></v-list-item>
+        <v-list-item to='/livers' title="登録ライバー"></v-list-item>
         <v-list-item to='/regist' title="新規登録" v-if="!st.loggedin"></v-list-item>
-        <v-list-item @click="logout" title="ログアウト"></v-list-item>
+        <LogoutComponent></LogoutComponent>
     </template>
     <v-pull-to-refresh
         :pull-down-threshold="pullDownThreshold"
---
+++ resources/js/pages/LiverView.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { VPullToRefresh } from 'vuetify/labs/VPullToRefresh';
+import { useLoginState } from '../stores/LoginState';
+import MainLayout from '../layouts/MainLayout.vue'
+import LogoutComponent from '../components/LogoutComponent.vue';
+
+const st = useLoginState()
+
+const livers = ref([])
+
+const pullDownThreshold = ref(64)
+
+const lodaing = async () => {
+    if(st.loggedin){
+        await axios.get('/api/livers')
+        .then((res) => {
+            console.log(res)
+            livers.value = res.data.detail
+        }).catch((err) => {
+            console.log(err)
+            if((err.status == 401) || (err.status == 419)){
+                st.logout()
+            }
+            console.log('livers err:' + err)
+        })
+    }
+}
+
+const load = async ({done}) => {
+    console.log('loading')
+    lodaing()
+    await new Promise(resolve => setTimeout(() => resolve(), 2000))
+    console.log('load finish')
+    done('ok')
+}
+
+onMounted(async () => {
+    lodaing()
+})
+
+</script>
+
+<template>
+<MainLayout title="登録ライバー">
+    <template v-slot:navigation>
+        <v-list-item to='/' title="ホーム"></v-list-item>
+        <v-list-item to='/livers' title="登録ライバー"></v-list-item>
+        <v-list-item to='/regist' title="新規登録" v-if="!st.loggedin"></v-list-item>
+        <LogoutComponent></LogoutComponent>
+    </template>
+
+    <v-container>
+        <v-pull-to-refresh
+        :pull-down-threshold="pullDownThreshold"
+        @load="load"
+        >
+            <v-list
+                density="compact"
+                :key="renderKey"
+            >
+                <v-list-item
+                    v-for="liver in livers"
+                >
+                    <v-list-item-title>
+                        <v-icon icon="mdi-account" class="d-inline"></v-icon>
+                        <a :href="liver.url" target="_blank" class="d-inline">
+                            {{ liver.name }}
+                        </a>
+                    </v-list-item-title>
+                    <v-list
+                        density="compact"
+                    >
+                        <v-list-item v-for="event in liver.events">
+                            {{ event.start_on }}〜{{ event.end_on }}：{{ event.name }}
+                        </v-list-item>
+                    </v-list>
+                </v-list-item>
+            </v-list>
+        </v-pull-to-refresh>
+    </v-container>
+
+</MainLayout>
+</template>
+
+<style>
+
+a {
+    text-decoration: none;
+    color: rgba(0, 0, 0, 87);
+}
+
+</style>
