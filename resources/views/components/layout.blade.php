281d0613135bd1c1fc816886d77c74b335b2e169
Toshio Abe <abtoc88@gmail.com>
2024-10-25 11:38:22 +0900

    OGPに画像を追加した

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,12 +8,14 @@
     <meta property="og:url" content="{{ route('client', ['client' => $id]) }}">
     <meta property="og:title" content="{{ $name }}様のページ">
     <meta property="og:description" content="Ayumi枠の相互のスクショを確認頂くページになります">
+    <meta property="og:image" content="{{ asset('/img/Ayumi_256.png') }}">
     <meta property="og:type" content="website">
     <meta name="twitter:card" content="app">
     <meta name="twitter:domain" content="system.ayumi.bar">
     <meta name="twitter:site" content="@ebi_ayu_01">
     <meta name="twitter:title" content="Ayumi枠の相互のスクショを確認頂くページになります">
     <meta name="twitter:description" content="Ayumi枠の相互のスクショを確認頂くページになります">
+    <meta name="twitter:image" content="{{ asset('/img/Ayumi_256.png') }}">
     <meta name="twitter:app:country" content="JP">
     <title>{{ $name }}様スクショ提供ページ</title>
     @vite(['resources/scss/app_c.scss', 'resources/js/app_c.js'])
